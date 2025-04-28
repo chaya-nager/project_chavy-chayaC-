73f506944142a3ed41ced61689e75e5d31780a79
chaya-nager <chaya4169225@gmail.com>
2025-04-28 16:50:45 +0300

    Working on "Service"

--- פרויקט גמר/Common/obj/Debug/net7.0/Common.AssemblyInfo.cs
+++ פרויקט גמר/Common/obj/Debug/net7.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2108e0b2c5879fa53c6552441ae4c64f7e889fcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ פרויקט גמר/Service/Services/MyMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Common.Dto;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class MyMapper:Profile
+    {
+        public MyMapper()
+        {
+            //CreateMap<User,UserDto>().ForMember("",x=>);
+        }
+    }
+}
